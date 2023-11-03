315fb2eda182139dc5713f75bdf0663be6603d05
Makito Katsube <katsubemakito@gmail.com>
2023-11-04 02:27:05 +0900

    /api/item/list.phpのデータをDBから取得するよう変更

--- public/api/item/list.php
+++ public/api/item/list.php
@@ -1,46 +1,41 @@
 <?php
 /**
- * 商品一覧を返す（モック）
+ * 商品一覧を返す
  *
  */
 
+//-----------------------------------------------
+// DBの接続情報
+//-----------------------------------------------
+$dsn = 'mysql:dbname=bukiyadb;host=localhost';
+$id  = 'senpai';			// DBのユーザー名
+$pw  = 'indocurry';		// DBのパスワード
+
+//-----------------------------------------------
+// DBから情報を取得
+//-----------------------------------------------
+$products = [ ];
+
+try {
+	// DBに接続
+	$dbh = new PDO($dsn, $id, $pw);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// SQLを準備して実行
+	$sth = $dbh->prepare('SELECT * FROM Products');		// 準備
+	$sth->execute();		// 実行
+
+	// DBから一括でデータを取得
+	$products = $sth->fetchAll(PDO::FETCH_ASSOC);
+}
+catch (PDOException $e) {
+	// エラー処理をここで行う
+}
+
+//-----------------------------------------------
+// JSON形式で結果を返す
+//-----------------------------------------------
 header('Content-Type: application/json');
 echo json_encode([
-	"items" => [
-		[
-			'id'    => 1,
-			'name'  => '木製ハンマー',
-			'image' => '1.png',
-			'description' => '叩かれるとシャレにならないハンマー',
-			'price' => 100
-		],
-		[
-			'id'    => 2,
-			'name'  => '鉄の剣',
-			'image' => '2.png',
-			'description' => '名工にとて鍛えられたかのようなオーラを放つ剣',
-			'price' => 200
-		],
-		[
-			'id'    => 3,
-			'name'  => '火縄銃',
-			'image' => '3.png',
-			'description' => '通称「種子島」、かつて歴史を変えた銃',
-			'price' => 300
-		],
-		[
-			'id'    => 4,
-			'name'  => '鉄の籠手',
-			'image' => '4.png',
-			'description' => '殴られると痛気持ちいいと評判',
-			'price' => 400
-		],
-		[
-			'id'    => 5,
-			'name'  => '魔導書',
-			'image' => '5.png',
-			'description' => '敵を毛むくじゃらにし身動きを封じる魔導書',
-			'price' => 500
-		]
-	]
+	'items' => $products
 ]);
